c0378c445e826be52fcc2f8e8d5bc329fde4a6aa
ubuntu-learning0 <kaybolmusherkesicin@gmail.com>
2026-09-06 16:46:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Publishable keys are designed for browser use when RLS is enabled.
 // NEVER put a Supabase secret/service_role key in this file.
 window.UBUNTU_CONFIG = {
-  supabaseUrl: 'https://yxsolveeaihjnakvoymfe.supabase.co',
+  supabaseUrl: 'https://yxsoveeaihjnakvoymfe.supabase.co',
   supabasePublishableKey: 'sb_publishable_r97cmky6idvaMF8MaoGx5Q_re70-hPw'
 };
